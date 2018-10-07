36903521d3e18b2e04775b7cce1ab2c58af202c1
bahraa <bbahra98@gmail.com>
2018-10-07 12:15:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,36 +1,42 @@
-
-let eastWest =  ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
-
-class Driver {
-  constructor(name, startDate){
-    this.name = name
-    this.startDate = new Date(startDate)
+class Driver{
+  constructor(name, startDate ){
+  this.name=name
+  this.startDate=new Date(startDate)
   }
   yearsExperienceFromBeginningOf(year){
-    let endDate = new Date(year, 1, 1)
-    let totalYears = (endDate - this.startDate)/(365*24*60*60*1000)
-    return parseInt(totalYears)
+    return year-this.startDate.getFullYear();
   }
 }
+let driver = new Driver("Alfie", "Aug 9, 1995")
+driver.yearsExperienceFromBeginningOf(2018);
 
-class Route {
-  constructor(beginningLocation, endingLocation){
-    this.beginningLocation = beginningLocation
-    this.endingLocation = endingLocation
-  }
-  avenueToInteger(avenue){
-    return eastWest.indexOf(avenue)
+class Route{
+  constructor(beginningLocation, endingLocation ){
+  this.beginningLocation=beginningLocation;
+  this.endingLocation=endingLocation;
   }
   blocksTravelled(){
-    let horizontalDistance = this.avenueToInteger(this.endingLocation.horizontal) - this.avenueToInteger(this.beginningLocation.horizontal)
-    let verticalDistance = this.endingLocation.vertical - this.beginningLocation.vertical
-    return (Math.abs(horizontalDistance) + Math.abs(verticalDistance))
+    const numofblocktravelled = Math.abs(parseInt
+    (this.beginningLocation.vertical)-parseInt
+    (this.endingLocation.vertical));
+    
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+    
+        const avenue = Math.abs(eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal));
+        return numofblocktravelled + avenue;
   }
-  estimatedTime(peak){
-    if(peak){
-      return this.blocksTravelled()/2
-    } else {
-      return this.blocksTravelled()/3
+  
+  estimatedTime(min){
+    console.log(this.blocksTravelled())
+    if(min === undefined){
+    return this.blocksTravelled()/3;}
+    else{
+      return this.blocksTravelled()/2;
     }
   }
 }
+
+
+let route = new Route({horizontal: '1st Avenue', vertical: '34'}, {horizontal: 'Park', vertical: '45'})
+route.blocksTravelled()
+route.estimatedTime()
